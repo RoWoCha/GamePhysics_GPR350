d6b5673e8a33390637502be3a0183c073bbfe24a
Egor Fesenko <fesenkoegor239@gmail.com>
2020-11-08 18:26:32 +0300

    Adding GameManager, UIManager and PlayerController + sprites for objects

---
+++ Assignment_8Plus/Assets/Scripts/Particle2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x += 1;
+        transform.position = newPos;
+    }
+}
---
+++ Assignment_8Plus/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotationSpeed = 60;
+    public GameObject particle;
+
+    Transform bulletSpawnTransform;
+
+    void Start()
+    {
+        bulletSpawnTransform = transform.Find("BulletSpawn");
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            transform.Rotate(new Vector3(0, 0, -rotationSpeed * Time.deltaTime));
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(particle, bulletSpawnTransform);
+            GameManager.instance.score++;
+        }
+    }
+}
